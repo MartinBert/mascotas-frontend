1547b666b04ef6c0e398b318645bcc39e1a5f906
root <bertelloagustin@gmail.com>
2024-01-02 16:48:17 -0300

    fix cross origin of product images (added 'crossOrigin' attribute in OpenImage component)

--- src/components/generics/openImage/OpenImage.jsx
+++ src/components/generics/openImage/OpenImage.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-const OpenImage = ({alt, imageUrl}) => {
+const OpenImage = ({ alt, imageUrl }) => {
     return (
-        <a href={imageUrl} target='_blank' rel='noreferrer'>
-            <img style={{cursor: 'pointer'}} width='70' height='70' src={imageUrl} alt={alt}/>
+        <a
+            href={imageUrl}
+            rel='noreferrer'
+            target='_blank'
+        >
+            <img
+                alt={alt}
+                crossOrigin='anonymous'
+                height='70'
+                src={imageUrl}
+                style={{ cursor: 'pointer' }}
+                width='70'
+            />
         </a>
     )
 }
 
-export default OpenImage;
+export default OpenImage
--- src/services/uploader.js
+++ src/services/uploader.js
@@ -3,41 +3,41 @@ import FormData from 'form-data'
 // var FormData = require('form-data')
 
 const checkStorageStatus = (err) => {
-    if(err.status === 401 || err.status === 403){
+    if (err.status === 401 || err.status === 403) {
         localStorage.clear()
     }
 }
 
-const deleteImage = async(id) => {
-    const headers = {headers: {Authorization: localStorage.getItem('token'), 'Content-Type':'application/json'}}
-    try{
+const deleteImage = async (id) => {
+    const headers = { headers: { Authorization: localStorage.getItem('token'), 'Content-Type': 'application/json' } }
+    try {
         const response = await axios.delete(`${process.env.REACT_APP_API_REST}/uploads/${id}`, headers)
         return response.data
-    }catch(err){
+    } catch (err) {
         checkStorageStatus(err)
         console.error(err)
     }
 }
 
-const getImageUrl = async(id) => {
-    const headers = {headers: {Authorization: localStorage.getItem('token'), 'Content-Type':'application/json'}}
-    try{
+const getImageUrl = async (id) => {
+    const headers = { headers: { Authorization: localStorage.getItem('token'), 'Content-Type': 'application/json' } }
+    try {
         const response = await axios.get(`${process.env.REACT_APP_API_REST}/uploads/${id}`, headers)
         return response.data
-    }catch(err){
+    } catch (err) {
         checkStorageStatus(err)
         console.error(err)
     }
 }
 
-const uploadImage = async(data) => {
-    const headers = {headers: {Authorization: localStorage.getItem('token')}}
+const uploadImage = async (data) => {
+    const headers = { headers: { Authorization: localStorage.getItem('token') } }
     const bodyMultiPart = new FormData()
     bodyMultiPart.append('file', data)
-    try{
+    try {
         const response = await axios.post(`${process.env.REACT_APP_API_REST}/uploads`, bodyMultiPart, headers)
         return response.data
-    }catch(err){
+    } catch (err) {
         checkStorageStatus(err)
         console.error(err)
     }
